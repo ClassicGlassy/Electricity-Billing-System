a6725345e93cec142e499a141720ac3cc9155811
ClassicGlassy <info.lucifer2@gmail.com>
2023-05-02 21:25:26 +0545

    Simple Login GUI, Imported Sprites and OpenSans Font

---
+++ src/com/userinterface/login.java
@@ -0,0 +1,69 @@
+package com.userinterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class login extends JFrame {
+    public login() {
+        super("Login");     //Sets the title of the JFrame
+        setLayout(null);
+
+/*
+        TODO
+        Import "OpenSans" font into the project.
+        Import "User" image into the project.
+*/
+
+//        JPanel bgPanel Properties
+        JPanel bgPanel = new JPanel();
+        bgPanel.setBackground(Color.lightGray);
+        bgPanel.setLayout(null);
+        bgPanel.setBounds(0,0,360,480);
+
+//        JLabel Username Properties
+        JLabel usernameL = new JLabel("USERNAME");
+        usernameL.setBounds(40,175,180,25);
+        bgPanel.add(usernameL);
+
+//        JTextField Username Properties
+        JTextField usernameT = new JTextField();
+        usernameT.setBounds(40,205,270,25);
+        bgPanel.add(usernameT);
+
+//        JLabel Password Properties
+        JLabel passwordL = new JLabel("PASSWORD");
+        passwordL.setBounds(40,260,182,25);
+        bgPanel.add(passwordL);
+
+//        JPasswordField Password Properties
+        JPasswordField passwordT = new JPasswordField();
+        passwordT.setBounds(40,290,270,25);
+        bgPanel.add(passwordT);
+
+//        JButton Login Properties
+        JButton loginBtn = new JButton("LOGIN");
+        loginBtn.setBounds(110,360,140,50);
+        bgPanel.add(loginBtn);
+
+
+
+
+        add(bgPanel);           //Adds Background Panel to JFrame
+
+//        Parameters of JFrame
+        setSize(new Dimension(360,480));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+/*
+    REMOVE main function
+    MAIN Function only to INVOKE login constructor
+*/
+
+    public static void main(String[] args) {
+        login login = new login();
+    }
+
+}
