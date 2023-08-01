8783efc415b6709204fe7005ff49cedc6c99abaa
ClassicGlassy <info.lucifer2@gmail.com>
2023-08-01 23:55:52 +0545

    Finished manage user panel and frame

---
+++ src/com/userinterface/project/panels/editProfileFrame.java
@@ -0,0 +1,140 @@
+package com.userinterface.project.panels;
+
+import com.database.ConnectionProvider;
+import com.userinterface.components.buttonComponent;
+import com.userinterface.components.labelComponent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+
+public class editProfileFrame extends JFrame {
+    public editProfileFrame (String email){
+        super("Manage User");
+
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(15,20,15,20);
+        gbc.fill = GridBagConstraints.BOTH;
+
+        gbc.anchor = GridBagConstraints.LINE_START;
+        gbc.gridx = 0;
+        labelComponent nameL = new labelComponent("Name:",20);
+        gbc.gridy = 0;
+        panel.add(nameL,gbc);
+
+        labelComponent emailL = new labelComponent("Email:",20);
+        gbc.gridy = 1;
+        panel.add(emailL,gbc);
+
+        labelComponent meterL = new labelComponent("Meter No:",20);
+        gbc.gridy = 2;
+        panel.add(meterL,gbc);
+
+        labelComponent statusL = new labelComponent("Status:",20);
+        gbc.gridy = 3;
+        panel.add(statusL,gbc);
+
+
+        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.gridx = 1;
+        try {
+            Connection con;
+            ConnectionProvider _connectionProvider = new ConnectionProvider();
+            con = _connectionProvider.getConnection();
+            String query;
+            PreparedStatement statement;
+            ResultSet rs;
+
+            query = "SELECT meterno, count(meterno) FROM meterdetails;";
+            statement = con.prepareStatement(query);
+            rs = statement.executeQuery();
+            rs.next();
+            String[] meterOptions = new String[rs.getInt(2)];
+
+            query = "SELECT meterno FROM meterdetails;";
+            statement = con.prepareStatement(query);
+            rs = statement.executeQuery();
+            int i =0;
+            while(rs.next()){
+                meterOptions[i]= rs.getString("meterno");
+                i++;
+            }
+
+
+            query = "SELECT name, meterno, status FROM users where id = (SELECT userid FROM login where email = ?);";
+            statement = con.prepareStatement(query);
+            statement.setString(1, email);
+            rs = statement.executeQuery();
+
+            String[] statusList = {"Inactive", "Active"};
+
+            if(rs.next()){
+                labelComponent nameV = new labelComponent(rs.getString("name"),20);
+                gbc.gridy = 0;
+                panel.add(nameV,gbc);
+
+                labelComponent emailV = new labelComponent(email,20);
+                gbc.gridy = 1;
+                panel.add(emailV,gbc);
+
+                String currentM = rs.getString("meterno");
+
+                JComboBox meterV = new JComboBox(meterOptions);
+                meterV.setSelectedItem(currentM);
+                gbc.gridy = 2;
+                panel.add(meterV,gbc);
+
+                JComboBox statusV = new JComboBox(statusList);
+                statusV.setSelectedIndex(rs.getInt("status"));
+                gbc.gridy = 3;
+                panel.add(statusV,gbc);
+
+                buttonComponent saveBtn = new buttonComponent("Save",Color.WHITE, Color.green);
+                gbc.anchor = GridBagConstraints.CENTER;
+                gbc.gridx = 0;
+                gbc.gridy = 4;
+                saveBtn.addActionListener(e -> {
+                    saveUser((String) meterV.getSelectedItem(), statusV.getSelectedIndex(), email);
+                });
+                panel.add(saveBtn,gbc);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+
+
+
+
+
+        add(panel);
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        setVisible(true);
+    }
+
+    private void saveUser(String meterno, int status, String email){
+        String query = "UPDATE `users` SET `meterno` = ?, `status` = ? WHERE id = (SELECT userid FROM login where email = ?) ;";
+        try {
+            Connection con;
+            ConnectionProvider _connectionProvider = new ConnectionProvider();
+            con = _connectionProvider.getConnection();
+            PreparedStatement statement;
+
+            statement = con.prepareStatement(query);
+            statement.setString(1,meterno);
+            statement.setInt(2, status);
+            statement.setString(3, email);
+
+            if(statement.executeUpdate() == 1){
+                JOptionPane.showMessageDialog(null,"User Information Updated Successfully");
+            }
+        }
+        catch (SQLException e){
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+    }
+}
--- src/com/userinterface/project/panels/mnguserPanel.java
+++ src/com/userinterface/project/panels/mnguserPanel.java
@@ -1,16 +1,78 @@
 package com.userinterface.project.panels;
 
-import com.userinterface.components.labelComponent;
+import com.database.ConnectionProvider;
+import com.userinterface.components.Utility;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class mnguserPanel extends JPanel {
+public class mnguserPanel extends JScrollPane {
     public mnguserPanel(){
-        setLayout(new GridBagLayout());
+        setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-        labelComponent test = new labelComponent("Hello this is users Panel", 50);
-        add(test);
-        setVisible(true);
+        String[] columnNames = {"Name", "Email", "Meter No", "Status"};
+        Object[][] rowData = new Object[0][];
+
+        DefaultTableModel model = new DefaultTableModel(rowData, columnNames);
+
+
+        JTable table = new JTable(model) {
+            public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                return false;
+            }
+        };
+        table.setFocusable(false);
+        table.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent me) {
+                if (me.getClickCount() == 2) {     // to detect doble click events
+                    JTable target = (JTable)me.getSource();
+                    int row = target.getSelectedRow(); // select a row
+                    editProfileFrame frame = new editProfileFrame((String) table.getValueAt(row,1));
+                    frame.setLocationRelativeTo(null);
+                }
+            }
+        });
+
+        Utility util = new Utility();
+        table.setFont(util.convertToFont("font/OpenSans.ttf", 20));
+        table.setRowHeight(35);
+
+        try {
+            Connection con;
+            ConnectionProvider _connectionProvider = new ConnectionProvider();
+            con = _connectionProvider.getConnection();
+            String query;
+//            PreparedStatement statement;
+            ResultSet rs;
+
+            query = "SELECT U.name, L.email, U.meterno, U.status FROM users AS U LEFT JOIN login AS L ON U.id = L.userid WHERE L.acc_type = 1;";
+            Statement statement = con.createStatement();
+
+            rs = statement.executeQuery(query);
+            int i = 0;
+            while(rs.next()) {
+                String status = "Inactive";
+                if(rs.getInt("status") == 1)
+                    status = "Active";
+                model.insertRow(i, new Object[]{rs.getString("name"), rs.getString("email"), rs.getString("meterno"), status});
+                i++;
+            }
+
+            con.close();
+
+        }
+        catch (SQLException e){
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+
+        getViewport().add(table);
+
     }
+
 }
